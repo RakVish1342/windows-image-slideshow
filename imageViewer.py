0279a4e83b9a6eedc84e4e2aed55302ef658caf1
Rxth <rakshith.vishwanatha@gmail.com>
2020-07-13 21:56:45 -0700

    Text on the image

--- imageViewer.py
+++ imageViewer.py
@@ -1,4 +1,5 @@
-from PIL import Image, ExifTags
+from matplotlib import pyplot as plt
+from PIL import Image, ImageDraw, ImageFont, ExifTags
 import glob
 import os
 
@@ -14,3 +15,16 @@ for file in glob.glob(globFileType):
     exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
     comment = exif['XPComment'].decode('utf-16-le').split('\x00')[0] # OR just use .decode('utf-16')
     print(comment)
+
+
+    # img.show()
+
+    draw = ImageDraw.Draw(img)
+
+    font = ImageFont.truetype('arial.ttf', size=70)
+    (x, y) = (50, 50)
+    color = 'rgb(0, 0, 0)' # black color
+    draw.text((x, y), comment, fill=color, font=font)
+
+
+    img.show()
